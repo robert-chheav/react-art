fae2d450c47801e0b78e54f17e1ce5fd19e525b6
robert-chheav <robert.chheav@gmail.com>
2024-01-20 17:14:31 +0100

    changing --glabal setting

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
+      <h1>ReactArtTEST</h1>
       <p>A community of artists and art-lovers.</p>
     </header>
   );
